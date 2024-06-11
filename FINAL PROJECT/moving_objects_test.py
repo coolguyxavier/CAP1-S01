96ac17abd6d12fd26fb8eb6e5a7b8baf6070fd7c
coolguyxavier <162495845+coolguyxavier@users.noreply.github.com>
2024-06-11 13:54:02 -0600

    Update moving_objects_test.py

--- FINAL PROJECT/moving_objects_test.py
+++ FINAL PROJECT/moving_objects_test.py
@@ -5,12 +5,19 @@
 # - Developer Comments - #
 # here in this module, i'm gonna be making the projectiles move
 # hopefully i could create some sprites for them.
+# the plan for the dictionary is that for each pellet/beam spawn, it subtracts one
+# from the key's value
+# like if 10 pellets spawn, the Pellet Key has an updated value of 90.
 
 # - Imports - #
 
 import pygame
 from math import cos, sin, atan2
 from random import randint
+
+# - Lists / Dictionaries - #
+
+projectile_dict = {'Pellet': 100, 'Beam': 5}
 
 # - Pygame Initialization - #
 
@@ -19,15 +26,17 @@ pygame.init()
 # - Constants - #
 
 red = (255, 0, 0)
+blue = (0, 0, 255)
 black = (0, 0, 0)
 white = (255, 255, 255)
 font = pygame.font.Font(None, 35) # fontstyle, fontsize
-proj_speed = 5
 
-# - Variables - #
+# - Variables - 
 p_health = 100
 
 game = True
+
+num_of_pellets = 3
 
 # - Game Setting - #
 
@@ -47,6 +56,14 @@ class Player:
         self.rect.y = 375 # where the player spawns
         self.player_xpos, self.player_ypos = self.rect.x, self.rect.y
             
+    def display_info(self):
+        # health display text
+        healthtext_01 = font.render("Player Health", 1, white)
+        screen.blit(healthtext_01, (606, 50))
+        
+        healthtext_02 = font.render(f"{p_health}%", 1, white)
+        screen.blit(healthtext_02, (650, 75))
+        
     def move(self, x, y):
         self.rect.x += x # im pretty sure this code 
         self.rect.y += y # makes the player able to move
@@ -66,14 +83,6 @@ class Player:
         elif self.rect.bottom > 645: # floor of box
             self.rect.bottom = 645
             
-    def display_info(self):
-        # health display text
-        healthtext_01 = font.render("Player Health", 1, white)
-        screen.blit(healthtext_01, (606, 50))
-        
-        healthtext_02 = font.render(f"{p_health}%", 1, white)
-        screen.blit(healthtext_02, (650, 75))
-            
             
 class Projectile:
     def __init__(self, width, height, speed):
@@ -83,13 +92,13 @@ class Projectile:
         self.speed = speed
         
         self.rect = self.img.get_rect() # creates placeholder rect on top of the image
-        self.rect.x, self.rect.y = (randint(0, 800)), (randint(0, 800)) #random spawn location
+        self.rect.x, self.rect.y = (randint(0, 800)), (randint(0, 800)) #random spawn location^
         self.angle = atan2(player.rect.y - self.rect.y, player.rect.x - self.rect.x)
-        # ^ fun math stuff, but i never learned radians so youre just gonna have to trust me ^
-
+        
     def move(self):
         self.rect.x += self.speed * cos(self.angle) # but i know cos
         self.rect.y += self.speed * sin(self.angle) # and sin though, so that's cool
+
         
 class SmallRect(Projectile):
     def __init__(self, width, height, color, speed):
@@ -97,48 +106,94 @@ class SmallRect(Projectile):
         self.color = color
         self.img.fill(self.color)
         
+class ThickLong(Projectile):
+    def __init__(self, width, height, color, speed):
+        super().__init__(width, height, speed)
+        self.color = color
+        self.img.fill(color)
+        self.img = pygame.image.load('SPRITES/PROJECTILES/comp_beam.png')
+        self.rect = self.img.get_rect() # creates rect on the image for collision
+        
+        self.rect.x, self.rect.y = (randint(0, 800)), (randint(0, 800)) #random spawn location^
+         
 # - Create Object(s)
 
 player = Player()
-bullet_01 = SmallRect(30, 60, red, 6) # we can comment out
+pellet = SmallRect(30, 30, white, 10) # we can comment out
+beam = ThickLong(50, 800, blue, 6)
 
 # - Main Code - #
 
 while game == True:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            game = False
 
     keys = pygame.key.get_pressed()
-        
-    # WASD INPUT
     if keys[pygame.K_a]:
-        player.move(-5, 0) # 5 frames to the left
-        
+        player.move(-5, 0)
     if keys[pygame.K_d]:
-        player.move(5, 0) # 5 frames to the right
-        
+        player.move(5, 0)
     if keys[pygame.K_w]:
-        player.move(0, -5) # 5 frames up 
-        
+        player.move(0, -5)
     if keys[pygame.K_s]:
-        player.move(0, 5) # 5 frames down
-            
-    if pygame.Rect.colliderect(player.rect, bullet_01.rect):
-        p_health -= 1
-        
-    screen.fill(black)
-    screen.blit(player.img, player.rect)
-    screen.blit(bullet_01.img, bullet_01.rect)
-
-    bullet_01.move()
-    player.display_info()
+        player.move(0, 5)
         
     if p_health <= 0:
         deathtext = font.render("You died!", 1, white)
         screen.blit(deathtext, (350, 400))
         pygame.time.wait(4000)
         game = False
+        
+    # mini logic code 
+    if pygame.Rect.colliderect(player.rect, pellet.rect):
+        p_health -= 1
+        
+    if pygame.Rect.colliderect(player.rect, beam.rect):
+        p_health -= 1
+        
+    # if there is less than 5 pellets on the screen and there are some in the dict, spawn a new pellet
+    pellet.move()
+    if num_of_pellets < 5 and projectile_dict['Pellet'] > 0:
+        pellet = SmallRect(30, 30, white, 10)
+
+        projectile_dict['Pellet'] - 1
+        
+        # if pellet is off the top/bottom of the screen, create new
+        if pellet.rect.x < 0 or pellet.rect.x > 800:
+            num_of_pellets -= 1
+            pellet.rect.x, pellet.rect.y = randint(0, 800), randint(0, 800)
+            pellet = SmallRect(30, 30, white, 10)
+            num_of_pellets += 1
+            
+        if pellet.rect.y < 0 or pellet.rect.y > 800:
+            num_of_pellets -= 1
+            pellet.rect.x, pellet.rect.y = randint(0, 800), randint(0, 800)
+            pellet = SmallRect(30, 30, white, 10)
+            num_of_pellets += 1
+        
+    #beam.beam_move()   # i'll bring all this back once I'm able to spawn more pellets at a time
+    #if beam.rect.x < - 1005 or beam.rect.x > 1000:
+    #    beam.rect.x, beam.rect.y = randint(1000, 1250), randint(1000, 1250)
+    #    beam = ThickLong(50, 800, blue, 6)
+        
+    #if beam.rect.y < -1010 or beam.rect.y > 1000:
+     #   beam.rect.x, beam.rect.y = randint(1000, 1250), randint(1000, 1250)
+      #  beam = ThickLong(50, 800, blue, 6)
+        
+    # drawing code
+    screen.fill(black)
+    screen.blit(player.img, player.rect)
+    screen.blit(pellet.img, pellet.rect)    
+    screen.blit(beam.img, beam.rect) 
+    
+    player.display_info()
+    
+    #print(player.rect.x, player.rect.y) # comment out when not needed
             
     pygame.display.flip()
+    
     clock.tick(60)
+
